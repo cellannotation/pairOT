e3cc0e73868b3b5fd824d235c4be43fc09118361
Felix Fischer <fe.fischer@icloud.com>
2025-11-11 15:11:51 +0100

    Added simple unit test to test imports

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,5 +1,3 @@
-import pytest
-
 import pairot
 
 
@@ -7,34 +5,13 @@ def test_package_has_version():
     assert pairot.__version__ is not None
 
 
-@pytest.mark.skip(reason="This decorator should be removed when test passes.")
-def test_example():
-    assert 1 == 0  # This test is designed to fail.
+def test_imports():
+    from pairot.align import DatasetMapping
+    from pairot.pl import plot_cluster_distance, plot_cluster_mapping, plot_sankey
+    from pairot.pp import preprocess_adatas
 
-
-@pytest.mark.skip(reason="This decorator should be removed when test passes.")
-@pytest.mark.parametrize(
-    "transform,layer_key,max_items,expected_len,expected_substring",
-    [
-        # Test default parameters
-        (lambda vals: f"mean={vals.mean():.2f}", None, 100, 1, "mean="),
-        # Test with layer_key
-        (lambda vals: f"mean={vals.mean():.2f}", "scaled", 100, 1, "mean=0."),
-        # Test with max_items limit (won't affect single item)
-        (lambda vals: f"max={vals.max():.2f}", None, 1, 1, "max=6.70"),
-    ],
-)
-def test_elaborate_example_adata_only_simple(
-    adata,  # this tests uses the adata object from the fixture in the conftest.py
-    transform,
-    layer_key,
-    max_items,
-    expected_len,
-    expected_substring,
-):
-    result = pairot.pp.elaborate_example(
-        items=[adata], transform=transform, layer_key=layer_key, max_items=max_items
-    )
-
-    assert len(result) == expected_len
-    assert expected_substring in result[0]
+    assert preprocess_adatas is not None
+    assert DatasetMapping is not None
+    assert plot_cluster_distance is not None
+    assert plot_cluster_mapping is not None
+    assert plot_sankey is not None
