2b4d6b12ce8b854da04513abd5080c104a00a811
Felix Fischer <fe.fischer@icloud.com>
2025-11-14 10:58:38 +0100

    Fixed docstring formatting

--- src/pairot/align/_dataset_ot.py
+++ src/pairot/align/_dataset_ot.py
@@ -458,10 +458,8 @@ class DatasetMapping:
                 If < 1, defines how much unbalanced the problem is on the second marginal.
             marginals_distribution
                 Whether the marginals should be uniform or balanced by cell-type frequency.
-                Use "uniform" for uniform marginals.
-                    (E.g. each cell contributes the same mass to the marginal distribution)
-                Use "balanced" for marginals balanced by cell-type frequency.
-                    (E.g. each cell type contributes the same mass to the marginal distribution)
+                Use "uniform" for uniform marginals. Meaning, each cell contributes the same mass to the marginal distribution.
+                Use "balanced" for marginals balanced by cell-type frequency. Meaning, each cell-type contributes the same mass to the marginal distribution.
                 This parameter is ignored, if marginals 'a' or 'b' are supplied via the **kwargs.
             kwargs
                 Keyword arguments passed to :class:`ott.problems.linear.linear_problem.LinearProblem`.
